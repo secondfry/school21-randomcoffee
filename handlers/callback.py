cef9ac64fb84dc7a0fe8d9db497e0aa9f91a9d6f
Rustam SecondFry Gubaydullin <secondfry+github@gmail.com>
2022-10-16 18:05:23 +0300

    Disallow setting match accepted if no match present

--- handlers/callback.py
+++ handlers/callback.py
@@ -71,6 +71,9 @@ async def set_active(
 
 
 async def set_accepted(upd: telegram.Update, ctx: telegram_ext.CallbackContext):
+    if not ctx.user_data.get(KEY_MATCH_WITH, None):
+        return
+
     ctx.user_data[KEY_MATCH_ACCEPTED] = True
     kbd = make_accepted_keyboard(get_accepted())
     await upd.callback_query.edit_message_reply_markup(
